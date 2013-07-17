28b8b549d4fbc5cc3f694e07028f804913bcac75
Seiya Tokui <beam.web@gmail.com>
2013-07-17 21:12:34 +0900

    Support aggregate_by

--- maf.py
+++ maf.py
@@ -84,7 +84,9 @@ class ExperimentContext(waflib.Build.BuildContext):
             call_object.rule = lambda task: rule_impl(task)
 
         if 'for_each' in call_object.__dict__:
-            self._generate_aggregation_tasks(call_object)
+            self._generate_aggregation_tasks(call_object, 'for_each')
+        elif 'aggregate_by' in call_object.__dict__:
+            self._generate_aggregation_tasks(call_object, 'aggregate_by')
         else:
             self._generate_tasks(call_object)
 
@@ -150,10 +152,11 @@ class ExperimentContext(waflib.Build.BuildContext):
         self._call_super(
             physical_call_object, source_parameter, target_parameter)
 
-    def _generate_aggregation_tasks(self, call_object):
+    def _generate_aggregation_tasks(self, call_object, key_type):
         # In aggregation tasks, source and target must be only one (meta) node.
         # Source node must be meta node. Whether target node is meta or not is
-        # automatically decided by source parameters and for_each keys.
+        # automatically decided by source parameters and for_each/aggregate_by
+        # keys.
         if not call_object.source or len(call_object.source) > 1:
             raise InvalidMafArgumentException(
                 "'source' in aggregation must include only one meta node")
@@ -169,8 +172,14 @@ class ExperimentContext(waflib.Build.BuildContext):
         target_to_source = collections.defaultdict(set)
 
         for source_parameter in source_parameters:
-            target_parameter = Parameter(
-                [(key, source_parameter[key]) for key in call_object.for_each])
+            target_parameter = Parameter()
+            if key_type == 'for_each':
+                for key in call_object.for_each:
+                    target_parameter[key] = source_parameter[key]
+            elif key_type == 'aggregate_by':
+                for key in source_parameter:
+                    if key not in call_object.aggregate_by:
+                        target_parameter[key] = source_parameter[key]
             target_to_source[target_parameter].add(source_parameter)
 
         for target_parameter in target_to_source:
@@ -185,7 +194,10 @@ class ExperimentContext(waflib.Build.BuildContext):
             physical_call_object = copy.deepcopy(call_object)
             physical_call_object.source = source
             physical_call_object.target = target
-            del physical_call_object.for_each
+            if key_type == 'for_each':
+                del physical_call_object.for_each
+            else:
+                del physical_call_object.aggregate_by
 
             self._call_super(
                 physical_call_object, source_parameter, target_parameter)
